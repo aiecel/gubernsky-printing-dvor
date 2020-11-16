dcc79fb01b90bdee1e91fbe55bfe987d5b158817
aiecel <170088@edu.psuti.ru>
2020-11-16 18:45:17 +0400

    Initial project structure

---
+++ src/main/java/com/aiecel/gubernskyprintingdvor/GubernskyPrintingDvorApplication.java
@@ -0,0 +1,13 @@
+package com.aiecel.gubernskyprintingdvor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GubernskyPrintingDvorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GubernskyPrintingDvorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aiecel/gubernskyprintingdvor/GubernskyPrintingDvorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiecel.gubernskyprintingdvor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GubernskyPrintingDvorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
